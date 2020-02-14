8e7ca90b28a5cf6db2101d4f00be97b83acb4780
vadimbvn <Sky4nii@yandex.com>
2020-02-15 01:50:48 +0300

    Внёс правки

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,10 @@
+/* eslint-disable */
 'use strict';
 
-var WIZARD_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARD_COUNT = 4;
 
 var userDialog = document.querySelector('.setup');
@@ -13,19 +14,14 @@ var similarListElement = document.querySelector('.setup-similar-list');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-var getRandomNumber = function (min, max) {
-  var rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-};
-
 var getWizards = function (wizardCount) {
   var wizards = [];
   for (var i = 0; i < wizardCount; i++) {
-    wizards[i] = {
-      name: WIZARD_NAME[getRandomNumber(0, WIZARD_NAME.length)] + ' ' + WIZARD_SURNAME[getRandomNumber(0, WIZARD_SURNAME.length)],
-      coatColor: WIZARD_COAT_COLOR[getRandomNumber(0, WIZARD_COAT_COLOR.length)],
-      eyesColor: WIZARD_EYES_COLOR[getRandomNumber(0, WIZARD_EYES_COLOR.length)]
-    };
+    wizards.push({
+      name: WIZARD_NAMES[getRandomNumber(0, WIZARD_NAMES.length)] + ' ' + WIZARD_SURNAMES[getRandomNumber(0, WIZARD_SURNAMES.length)],
+      coatColor: WIZARD_COAT_COLORS[getRandomNumber(0, WIZARD_COAT_COLORS.length)],
+      eyesColor: WIZARD_EYES_COLORS[getRandomNumber(0, WIZARD_EYES_COLORS.length)]
+    });
   }
   return wizards;
 };
